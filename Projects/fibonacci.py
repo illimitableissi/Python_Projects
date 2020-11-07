9a4233c57a8b9278367b77963b7b86ce1744fb40
SOVEREIGN <isimemeni@gmail.com>
2020-11-07 16:57:13 -0500

    added e formula

---
+++ Projects/fibonacci.py
@@ -0,0 +1,7 @@
+def fibonacci(n):
+    """
+    Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
+    """
+    a = 0
+    b = 1
+    
--- Projects/pi.py
+++ Projects/pi.py
@@ -12,3 +12,14 @@ pi_calculate(2)
 pi_calculate(15)
 pi_calculate(40)
 pi_calculate(4)
+
+def e_calculate(n):
+    """
+    Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
+    """
+    print(f"%.{n}f" % math.e)
+
+e_calculate(2)
+e_calculate(15)
+e_calculate(40)
+e_calculate(4)

38c9ceb09e7ba57629fee60d71a74c16c1405262
SOVEREIGN <isimemeni@gmail.com>
2020-11-09 22:26:36 -0500

    working on change app

---
+++ Projects/change.py
@@ -0,0 +1,5 @@
+def chaneg(n):
+    """
+    The user enters a cost and then the amount of money given. The program will figure out the change and the number of quarters, dimes, nickels, pennies needed for the change.
+    """
+    pass
